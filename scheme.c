2f022df353c3787c2d41d82c475591d7890ab931
Aditya Potluri <adityapotluri1@gmail.com>
2020-08-06 23:09:15 -0400

    scheme objects in place

--- scheme.c
+++ scheme.c
@@ -1,6 +1,36 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<stdbool.h>
+#include<ctype.h>
+#include<string.h>
 
+typedef enum {FIXNUM} object_type;
+typedef struct object {
+    object_type type;
+    union {
+        struct {
+            long value;
+        } fixnum;
+    } data;
+} object;
+
+object *alloc_object(void) {
+    object *obj;
+    obj = malloc(sizeof(object));
+    if(obj == NULL) {
+        fprintf(stderr, "Out of Memory Lol\n");
+        exit(1);
+    }
+    return obj;
+}
+
+object *make_fixnum(long value) {
+    object* obj;
+    obj = alloc_object();
+    obj->type = FIXNUM;
+    obj->data.fixnum.value = value;
+    return obj;
+}
 bool isInteger(char c) {
     int digits[10] = {0,1,2,3,4,5,6,7,8,9};
     for(int i = 0; i <10; i++) {
@@ -13,10 +43,9 @@ bool isInteger(char c) {
 
 
 int main() {
-    char c;
-    while((c = getchar()) != EOF) {
-        printf("%d ",isInteger(c));
-    }
+   while(1) {
+
+   }
 }
 
 
