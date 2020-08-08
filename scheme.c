af24066664954a7fd15b570646a156f22665a3e2
Aditya Potluri <adityapotluri1@gmail.com>
2020-08-08 11:11:11 -0400

    booleans done

--- scheme.c
+++ scheme.c
@@ -5,7 +5,7 @@
 #include<string.h>
 
 // defines all the different built in types in scheme
-typedef enum {FIXNUM} object_type;
+typedef enum {FIXNUM,BOOLEAN} object_type;
 
 // defines the structure for all the different objects and their data
 typedef struct object {
@@ -14,8 +14,15 @@ typedef struct object {
         struct {
             long value;
         } fixnum;
+
+        struct {
+            bool value;
+        } boolean;
     } data;
 } object;
+
+
+
 
 // allocates memory for the object (no garbage collection)
 object* alloc_object(void) {
@@ -28,6 +35,14 @@ object* alloc_object(void) {
     return obj;
 }
 
+object* make_boolean(bool b) {
+    object* obj;
+    obj = alloc_object();
+    obj->type = BOOLEAN;
+    obj->data.fixnum.value = b;
+}
+
+
 // the "constructor" for the fixnum type (integers basically)
 object* make_fixnum(long value) {
     object* obj;
@@ -37,7 +52,17 @@ object* make_fixnum(long value) {
     return obj;
 }
 
+bool isBool(object *obj) {
+    return obj->type == BOOLEAN;
+}
 
+bool isTrue(object *obj) {
+    return obj->data.boolean.value == true;
+}
+
+bool isFalse(object *obj) {
+    return obj->data.boolean.value == false;
+}
 
 //just says if its a fixnum
 bool isFixNum(object *obj) {
@@ -77,6 +102,18 @@ void eatWhitespace(FILE *in) {
     
 }
 
+
+object *false_val;
+object *true_val;
+void init() {
+    false_val = alloc_object();
+    false_val->type = BOOLEAN;
+    false_val->data.boolean.value = false;
+
+    true_val = alloc_object();
+    true_val->type  = BOOLEAN;
+    true_val->data.boolean.value  = true;
+}
 // The read in Read Evalute Print Loop
 object* read(FILE *in) {
     int c;
@@ -108,6 +145,20 @@ object* read(FILE *in) {
             exit(1);
         }
     }
+
+    if(c == '#') {
+        c = getc(in);
+        switch (c)
+        {
+        case 't':
+            return true_val;
+        case 'f':
+            return false_val;
+        default:
+            fprintf(stderr, "unknown boolean literal\n");
+            exit(1);
+        }
+    }
     else {
         fprintf(stderr, "bad input. Unexpected '%c'\n", c);
         exit(1);
@@ -127,6 +178,8 @@ void write(object *obj) {
         case FIXNUM:
             printf("%ld", obj->data.fixnum.value);
             break;
+        case BOOLEAN:
+            printf("#%c ", obj->data.boolean.value ? 't' : 'f' );
         default:
             fprintf(stderr, "cannot write unknown type\n");
             exit(1);
@@ -136,6 +189,7 @@ void write(object *obj) {
 
 int main(void) {
    printf("Scheme Bootstrap! press Ctrl ^c to to exit\n");
+   init();
    while(1) {
        printf(">");
        write(eval(read(stdin)));
